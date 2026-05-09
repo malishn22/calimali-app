4b1530359f6d3bd00d9791e1c02375eb3876cc1b
Mali Sahin <mali.sahin2222@gmail.com>
2026-05-09 22:24:13 +0300

    fix: fixes

--- app/session-detail.tsx
+++ app/session-detail.tsx
@@ -3,7 +3,7 @@ import { SessionHistory } from "@/constants/Types";
 import { Feather } from "@expo/vector-icons";
 import { useLocalSearchParams, useRouter } from "expo-router";
 import React from "react";
-import { Platform, Pressable, StatusBar, View } from "react-native";
+import { Platform, Pressable, StatusBar, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function SessionDetailScreen() {
